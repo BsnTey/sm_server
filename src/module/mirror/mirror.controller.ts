400447af6ecde8dfe7fe9e4c44471b321abc4ee6
BsnTey <kirill2494@mail.ru>
2024-12-17 18:56:37 +0300

    del mirror

--- src/module/mirror/mirror.controller.ts
+++ src/module/mirror/mirror.controller.ts
@@ -1,8 +1,5 @@
-import { Controller, Get, HttpCode, Param, Res } from '@nestjs/common';
-import { AccountIdParamsDto } from '../account/dto/uuid-account.dto';
-import { Response } from 'express';
+import { Controller } from '@nestjs/common';
 import { AccountService } from '../account/account.service';
-import { Cookie } from '../account/interfaces/cookie.interface';
 import { ConfigService } from '@nestjs/config';
 
 @Controller('mirror')
@@ -14,31 +11,31 @@ export class MirrorController {
         private configService: ConfigService,
     ) {}
 
-    @Get(':accountId')
-    @HttpCode(200)
-    async onMirror(@Param() params: AccountIdParamsDto, @Res({ passthrough: true }) res: Response) {
-        const accountEntity = await this.accountService.getAccount(params.accountId);
-        const cookies: Cookie[] = JSON.parse(accountEntity.cookie);
-        const domain = this.DOMAIN.split('://')[1];
-
-        const smid = cookies.find(cookie => {
-            if (cookie.name == 'SMID') return true;
-        });
-
-        res.clearCookie('SMID');
-
-        const twoYearsFromNow = new Date();
-        twoYearsFromNow.setFullYear(twoYearsFromNow.getFullYear() + 2);
-
-        res.cookie('SMID', smid!.value, {
-            domain,
-            httpOnly: true,
-            path: '/',
-            sameSite: 'lax',
-            secure: true,
-            expires: twoYearsFromNow,
-        });
-
-        return res.redirect('/');
-    }
+    // @Get(':accountId')
+    // @HttpCode(200)
+    // async onMirror(@Param() params: AccountIdParamsDto, @Res({ passthrough: true }) res: Response) {
+    //     const accountEntity = await this.accountService.getAccount(params.accountId);
+    //     const cookies: Cookie[] = JSON.parse(accountEntity.cookie);
+    //     const domain = this.DOMAIN.split('://')[1];
+    //
+    //     const smid = cookies.find(cookie => {
+    //         if (cookie.name == 'SMID') return true;
+    //     });
+    //
+    //     res.clearCookie('SMID');
+    //
+    //     const twoYearsFromNow = new Date();
+    //     twoYearsFromNow.setFullYear(twoYearsFromNow.getFullYear() + 2);
+    //
+    //     res.cookie('SMID', smid!.value, {
+    //         domain,
+    //         httpOnly: true,
+    //         path: '/',
+    //         sameSite: 'lax',
+    //         secure: true,
+    //         expires: twoYearsFromNow,
+    //     });
+    //
+    //     return res.redirect('/');
+    // }
 }
