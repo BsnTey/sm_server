456acaee3c5a7a995893b61840f7b3d35440a9cb
BsnTey <kirill2494@mail.ru>
2025-02-13 13:41:59 +0300

    add try catch for http

--- src/module/http/http.service.ts
+++ src/module/http/http.service.ts
@@ -1,20 +1,37 @@
 import axios, { AxiosResponse } from 'axios';
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { IHttpInterface } from './interfaces/http.interface';
 
 @Injectable()
 export class HttpService implements IHttpInterface {
+    private timeout = 10000;
+
     constructor() {}
 
     async get<T = any>(url: string, options: any = {}): Promise<AxiosResponse<T>> {
-        return axios.get<T>(url, options);
+        try {
+            return await axios.get<T>(url, { timeout: this.timeout, ...options });
+        } catch (error: any) {
+            console.error(`HTTP GET error on url ${url}:`, error.message);
+            throw new InternalServerErrorException(`Ошибка HTTP GET запроса: ${error.message}`);
+        }
     }
 
     async post<T = any>(url: string, payload: any, options: any = {}): Promise<AxiosResponse<T>> {
-        return await axios.post<T>(url, payload, options);
+        try {
+            return await axios.post<T>(url, payload, { timeout: this.timeout, ...options });
+        } catch (error: any) {
+            console.error(`HTTP POST error on url ${url}:`, error.message);
+            throw new InternalServerErrorException(`Ошибка HTTP POST запроса: ${error.message}`);
+        }
     }
 
     async delete<T = any>(url: string, options: any = {}): Promise<AxiosResponse<T>> {
-        return await axios.delete<T>(url, options);
+        try {
+            return await axios.delete<T>(url, { timeout: this.timeout, ...options });
+        } catch (error: any) {
+            console.error(`HTTP DELETE error on url ${url}:`, error.message);
+            throw new InternalServerErrorException(`Ошибка HTTP DELETE запроса: ${error.message}`);
+        }
     }
 }
