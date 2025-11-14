27c90e0c5084905fe6a2f5a3e2835bc275f605a4
BsnTey <kirill2494@mail.ru>
2025-11-14 18:09:21 +0300

    open my discount checker

--- src/module/telegram/keyboards/profile.keyboard.ts
+++ src/module/telegram/keyboards/profile.keyboard.ts
@@ -8,7 +8,7 @@ export const profileKeyboard = (role: UserRole) => {
     role != UserRole.User ? btnArr.push([Markup.button.callback('Призы фортуны', 'fortune')]) : null;
     // role != 'User' ? btnArr.push([Markup.button.callback('Получить промокод для бота продаж', 'get_promocode')]) : null;
     btnArr.push([Markup.button.callback('Чекер промо', 'check_promo')]);
-    role == UserRole.Admin ? btnArr.push([Markup.button.callback('Чекер моей скидки', 'check_my_discount')]) : null;
+    btnArr.push([Markup.button.callback('Чекер моей скидки', 'check_my_discount')]);
     btnArr.push([Markup.button.callback('Получить инфо по заказу', 'get_info_order')]);
     return Markup.inlineKeyboard(btnArr);
 };
